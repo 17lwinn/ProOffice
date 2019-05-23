9cbcb824ac1ff9f8c3f804b974a14505a482c231
17lwinn <50519725+17lwinn@users.noreply.github.com>
2019-05-23 13:12:05 +0100

    Update proOffice text document reader.py

--- proOffice file tools/proOffice text document reader.py
+++ proOffice file tools/proOffice text document reader.py
@@ -1,10 +1,14 @@
+
+
 from time import *
 
+print("welcome to proTech TXT file reader")
+sleep(2)
 file = input("please type in the full name and extension of the file you want to read: ")
 
 f = open(file, "r")
 
 print(f.read(10000))
-sleep(5)
+sleep(21)
 
 

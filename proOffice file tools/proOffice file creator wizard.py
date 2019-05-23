3ca6fc9854a22395c20b25e4a45921595ef0a56b
17lwinn <50519725+17lwinn@users.noreply.github.com>
2019-05-23 13:11:35 +0100

    Update proOffice file creator wizard.py

--- proOffice file tools/proOffice file creator wizard.py
+++ proOffice file tools/proOffice file creator wizard.py
@@ -1,14 +1,16 @@
 # this software copy includes an MIT licence- see our license file for more info
 
- 
+ from time import *
 
-from time import *
-
+print("welcome to ProTech file creator")
+sleep(2)
 file = input("please type in the full name and extension of the file you want to create: ")
 
 f = open(file, "x")
 
 print(f.read(10000))
 sleep(5)
+
+
 
 

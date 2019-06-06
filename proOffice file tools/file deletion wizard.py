578ce0101d0904729285ae62687b374dfc735399
17lwinn <50519725+17lwinn@users.noreply.github.com>
2019-06-06 13:05:12 +0100

    Update file deletion wizard.py

--- proOffice file tools/file deletion wizard.py
+++ proOffice file tools/file deletion wizard.py
@@ -1,5 +1,11 @@
 from time import *
+print("to prevent software piracy, we will now search your DOCUMENTS folder for the license.txt file.")
+sleep(5)
+f = open("LICENSE.txt", "r")
 
+print(f.read(100000000))
+sleep(3)
+print("____________________________________________________________")
 
 file = input("please enter the name of the file pending deletion: ")
 

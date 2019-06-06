0cf09d827acbb725c9a33d18ceac1fd76f402a5d
17lwinn <50519725+17lwinn@users.noreply.github.com>
2019-06-06 13:05:39 +0100

    Update proOffice file creator wizard.py

--- proOffice file tools/proOffice file creator wizard.py
+++ proOffice file tools/proOffice file creator wizard.py
@@ -1,7 +1,13 @@
 # this software copy includes an Mozilia Public License- see our license file for more info
 
- from time import *
+from time import *
+print("to prevent software piracy, we will now search your DOCUMENTS folder for the license.txt file.")
+sleep(5)
+f = open("LICENSE.txt", "r")
 
+print(f.read(100000000))
+sleep(3)
+print("____________________________________________________________")
 print("welcome to ProTech file creator")
 sleep(2)
 file = input("please type in the full name and extension of the file you want to create: ")

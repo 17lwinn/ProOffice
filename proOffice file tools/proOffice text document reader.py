f51744554fea93934a9140d9dd359d6fcf54cf43
17lwinn <50519725+17lwinn@users.noreply.github.com>
2019-06-06 13:06:01 +0100

    Update proOffice text document reader.py

--- proOffice file tools/proOffice text document reader.py
+++ proOffice file tools/proOffice text document reader.py
@@ -2,7 +2,13 @@
 
 
 from time import *
+print("to prevent software piracy, we will now search your DOCUMENTS folder for the license.txt file.")
+sleep(5)
+f = open("LICENSE.txt", "r")
 
+print(f.read(100000000))
+sleep(3)
+print("____________________________________________________________")
 print("welcome to proTech TXT file reader")
 sleep(2)
 file = input("please type in the full name and extension of the file you want to read: ")
